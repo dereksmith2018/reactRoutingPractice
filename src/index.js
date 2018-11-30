a5627596b72a9923bb3526f3a36680fdea8efa7e
Derek Smith <dereksmith@Dereks-MacBook-Air.local>
2018-11-29 19:12:38 -0800

    init starting with routing

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowerRouter } from "react-router-dom";
+import "./index.css";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
+
+ReactDOM.render(
+  <BrowerRouter>
+    <App />
+  </BrowerRouter>,
+  document.getElementById("root")
+);
+registerServiceWorker();
